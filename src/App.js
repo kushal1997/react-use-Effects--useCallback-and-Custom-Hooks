d70bf63e0ed1ade3d5a4e78bfbd63b3158a28bad
kushal1997 <kushalrao103@gmail.com>
2023-03-23 20:10:56 +0530

    learned how to use useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import { ProductList } from './components/ProductList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Product List</h1>
+      <ProductList/>
     </div>
   );
 }
---
+++ src/components/ProductList.js
@@ -0,0 +1,15 @@
+import { useState ,useEffect} from "react"
+
+export const ProductList = () => {
+    const [products,setProducts]=useState([]);
+    console.log(products);
+    useEffect(()=>{
+        fetch("http://localhost:8000/products")
+        .then(response=>response.json())
+        .then(data=>setProducts(data));
+    },[])
+    
+  return (
+    <div>ProductList</div>
+  )
+}

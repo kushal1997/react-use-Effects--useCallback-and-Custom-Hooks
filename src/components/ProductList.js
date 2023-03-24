d1d32ad1bbd0a5f6d095c8584be22e38f2544124
kushal1997 <kushalrao103@gmail.com>
2023-03-24 06:46:35 +0530

    onClick Event

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,15 +2,24 @@ import { useState ,useEffect} from "react"
 
 export const ProductList = () => {
     const [products,setProducts]=useState([]);
-    
+    const[url,setUrl]=useState("http://localhost:8000/products");
+    const [counter,setCounter]=useState(0);
     useEffect(()=>{
-        fetch("http://localhost:8000/products")
+        fetch(url)
         .then(response=>response.json())
         .then(data=>setProducts(data));
-    },[])
+    },[url])
+    useEffect(()=>{
+      console.log(counter);
+    },[counter])
     
   return (
     <section>
+      <div className="filter">
+        <button onClick={()=>setCounter(counter+1)}>{counter}</button>
+        <button className="all" onClick={()=> setUrl("http://localhost:8000/products")}>All</button>
+        <button className="onlyAvailable" onClick={()=>setUrl("http://localhost:8000/products?in_stock=true")}>Available</button>
+      </div>  
       {products.map((product)=>(
         <div className="card" key={product.id}>
           <p className="id">{product.id}</p>

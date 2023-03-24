cb1cc12f963ae0c5ee198064f72960b1026f8525
kushal1997 <kushalrao103@gmail.com>
2023-03-24 09:52:40 +0530

    loading state

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,6 @@
 import { useState ,useEffect, useCallback} from "react"
 import { useFetch } from "../hooks/useFetch";
+import Loading from "../assets/loading.gif"
 export const ProductList = () => {
     
     const[url,setUrl]=useState("http://localhost:8000/products");
@@ -20,7 +21,7 @@ export const ProductList = () => {
     //     setProducts(data);
     //   },[url]);
 
-    const {data:products}=useFetch(url);
+    const {data:products,loading}=useFetch(url);
     
     
     // useEffect( ()=>{
@@ -38,6 +39,8 @@ export const ProductList = () => {
         <button className="all" onClick={()=> setUrl("http://localhost:8000/products")}>All</button>
         <button className="onlyAvailable" onClick={()=>setUrl("http://localhost:8000/products?in_stock=true")}>Available</button>
       </div>  
+      {loading && <p className="loading"><img src={Loading} alt="" /></p>}
+      
       {products && products.map((product)=>(
         <div className="card" key={product.id}>
           <p className="id">{product.id}</p>

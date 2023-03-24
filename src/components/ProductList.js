d50db35c0e82f6fcb4a54af942dd26f47d25b3e6
kushal1997 <kushalrao103@gmail.com>
2023-03-24 08:49:53 +0530

    useCallback Hook

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,22 +1,38 @@
-import { useState ,useEffect} from "react"
+import { useState ,useEffect, useCallback} from "react"
 
 export const ProductList = () => {
     const [products,setProducts]=useState([]);
     const[url,setUrl]=useState("http://localhost:8000/products");
-    const [counter,setCounter]=useState(0);
-    useEffect(()=>{
-        fetch(url)
-        .then(response=>response.json())
-        .then(data=>setProducts(data));
-    },[url])
-    useEffect(()=>{
-      console.log(counter);
-    },[counter])
+    // useEffect(()=>{
+    //   console.log(counter);
+    // },[counter])
+
+    // const [counter,setCounter]=useState(0);
+    // useEffect(()=>{
+    //     fetch(url)
+    //     .then(response=>response.json())
+    //     .then(data=>setProducts(data));
+    // },[url])
+    const fetchProduct= useCallback(
+      async ()=>{
+        const response=await fetch(url);
+        const data=await response.json();
+        setProducts(data);
+      },[url]);
+    
+    
+    useEffect( ()=>{
+      
+      fetchProduct();
+      console.log("==============");
+    },[fetchProduct]);
+
+    
     
   return (
     <section>
       <div className="filter">
-        <button onClick={()=>setCounter(counter+1)}>{counter}</button>
+        {/* <button onClick={()=>setCounter(counter+1)}>{counter}</button> */}
         <button className="all" onClick={()=> setUrl("http://localhost:8000/products")}>All</button>
         <button className="onlyAvailable" onClick={()=>setUrl("http://localhost:8000/products?in_stock=true")}>Available</button>
       </div>  
